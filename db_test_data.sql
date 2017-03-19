3fd65a9e9008d8969cde64363b768330330ffa7d
mao <1126924038@qq.com>
2017-03-19 10:16:44 +0800

    【重要】添加测试数据脚本文件db_test_data

---
+++ db_test_data.sql
@@ -0,0 +1,15 @@
+#说明：
+#本文件为测试数据，直接在mysql命令行下，复制所有语句粘贴然后即可。
+
+
+#添加测试用户
+
+insert into user (username, password, phone) values ("诸葛亮", 123456, "15603001234");
+
+insert into user (username, password, phone) values ("李白666", 123456, "15603001235");
+
+insert into user (username, password, phone) values ("荆轲", 123456, "15603001236");
+
+insert into user (username, password, phone) values ("安琪拉", 123456, "15603001237");
+
+insert into user (username, password, phone) values ("后裔", 123456, "15603001238");
